f792fe08c5a475410c0cbbb0eca44d3a68eccf51
Prehaud Benjamin <benjiprehaud@gmail.com>
2025-12-19 11:48:09 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -113,7 +113,7 @@ def plot_referendum_map(referendum_result_by_regions):
     merged['ratio'] = merged['Choice A'] / (merged['Choice A'] +
                                             merged['Choice B'])
 
-    # gdf.plot()
+    gdf.plot()
 
     return gpd.GeoDataFrame(merged)
 
